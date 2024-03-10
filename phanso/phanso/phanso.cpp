25b97f8f21f9f7f041e9edb7d4ff1050b0a303b7
Huynh Dang <nguyendangchanell@gmail.com>
2024-03-10 22:56:45 +0700

    Bai 2 chuong 3

---
+++ phanso/phanso/phanso.cpp
@@ -0,0 +1,33 @@
+﻿// phanso.cpp : Defines the entry point for the application.
+//
+
+#include "phanso.h"
+
+using namespace std;
+
+int main()
+{
+	
+	phanso Phan_so1,Phan_so2;
+	//nhap phan so thu 2
+	cout << "Nhap phan so thu nhat "<<endl ;
+	Phan_so1.nhap_phanso();
+	cout << "Phan so thu nhat : ";
+	Phan_so1.hien_thi_phanso();
+	//nhap phan so thu 2
+	cout << endl; 
+	cout << "Nhap phan so thu hai " << endl;
+	Phan_so2.nhap_phanso();
+	cout << "Phan so thu hai : ";
+	Phan_so2.hien_thi_phanso();
+	cout << endl;
+	// Cộng hai phân số
+	phanso ket_qua = Phan_so1.cong(Phan_so2);
+
+	// Hiển thị kết quả
+	cout << "Tong hai phan so: ";
+	ket_qua.hien_thi_phanso();
+	
+	cout << endl;
+	return 0;
+}
---
+++ phanso/phanso/phanso.h
@@ -0,0 +1,35 @@
+﻿// phanso.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+#include <iostream>
+using namespace std; 
+class phanso {
+private:
+	int tu_so;
+	int mau_so;
+public :
+	//hàm setter
+	void set(int TS, int MS) {
+		tu_so = TS;
+		mau_so = MS;
+	}
+	void nhap_phanso() {
+		cout << "Nhap tu so : "; cin >> tu_so; 
+		do {
+			cout << "Nhap mau so ( khac 0 ): "; cin >> mau_so;
+		} while (mau_so == 0);
+	}
+	void hien_thi_phanso() {
+		cout << tu_so << "/" << mau_so;
+	}
+	// Hàm cộng hai phân số
+	phanso cong(const phanso& ps) const {
+		phanso ket_qua;
+		ket_qua.tu_so = tu_so * ps.mau_so + ps.tu_so * mau_so;
+		ket_qua.mau_so = mau_so * ps.mau_so;
+		return ket_qua;
+	}
+};
+// TODO: Reference additional headers your program requires here.
